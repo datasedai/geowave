7236c2f3bec5c8c284d7e600a8f7d418367e8d61
Derek Yeager <dcy2003@gmail.com>
2016-08-31 19:47:30 -0400

    wip

--- core/index/src/main/java/mil/nga/giat/geowave/core/index/lexicoder/Lexicoders.java
+++ core/index/src/main/java/mil/nga/giat/geowave/core/index/lexicoder/Lexicoders.java
@@ -9,5 +9,4 @@ public class Lexicoders
 	public static final ShortLexicoder SHORT = new ShortLexicoder();
 	public static final IntegerLexicoder INT = new IntegerLexicoder();
 	public static final LongLexicoder LONG = new LongLexicoder();
-	public static final DoubleLexicoder DOUBLE = new DoubleLexicoder();
 }
